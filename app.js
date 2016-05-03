bd1dd22f2b429c2455747e4f5e0da8f862d90a55
Moataz Ossama <moataz.m.ossama@gmail.com>
2016-05-03 16:33:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('temStore', [
   'ngRoute',
-  'temStore.view1',
-  'temStore.view2',
   'temStore.templates'
 ]).
 config(['$routeProvider', function($routeProvider) {
